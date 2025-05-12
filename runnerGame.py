7fb9c01acbca9efeaa96681448b11d6761c0e012
sanskriti <sanskritidutta641@gmail.com>
2025-05-12 11:43:41 +0530

    Runner Game

---
+++ runnerGame.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 11 00:00:25 2025
+
+@author: Sanskriti Dutta
+"""
+import pygame
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((800 , 400))
+
+pygame.display.set_caption("Runner Game")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+
+while True :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT :
+            pygame.quit()
+            exit()
+    screen.blit(test_surface, (0,0))
+    pygame.display.update()
+    clock.tick(60)
