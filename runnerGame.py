d94796182d20088275f332bf60f590c2b61e9434
sanskriti <sanskritidutta641@gmail.com>
2025-05-13 20:06:52 +0530

    runner

--- runnerGame.py
+++ runnerGame.py
@@ -1,27 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 11 00:00:25 2025
-
-@author: Sanskriti Dutta
-"""
+from matplotlib.font_manager import get_font
 import pygame
 from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((800 , 400))
-
+# Game screen  
+screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner Game")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+# Load image 
+ground_surface = pygame.image.load('ground.png')  
+# text_surface = pygame.image.load('My Game' , False , 'Black') 
 
-while True :
+while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT :
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(test_surface, (0,0))
+    screen.blit(ground_surface, (0, 0))
+    # screen.blit(text_surface, (300 , 50))
     pygame.display.update()
     clock.tick(60)
