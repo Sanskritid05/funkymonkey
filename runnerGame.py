f9c7ca1c5b16b357b7c1597bf003ee8fc57a0e3f
sanskriti <sanskritidutta641@gmail.com>
2025-05-14 12:05:27 +0530

    RunnerGame -d3

--- runnerGame.py
+++ runnerGame.py
@@ -11,15 +11,20 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('game_font.ttf', 50)
 
 # CHECKPOINT 2 --> Load image :: with multiple surfaces
-ground_surface = pygame.image.load('ground2.png') .convert() # convert() :: for efficient use w.r.t pygame  
-text_surface = test_font.render('My Game' , False , 'Black').convert() 
 
-snail_surface = pygame.image.load('snail(1).png').convert_alpha() # alpha removes unnecessary background
-snail_rectangle = snail_surface.get_rect(topright = (700,280))
+ground_surf = pygame.image.load('ground2.png') .convert() # convert() :: for efficient use w.r.t pygame  
 
 
-player_surface = pygame.image.load('gameCharacter.png').convert_alpha()
-player_rectangle = player_surface.get_rect(topleft = (80, 180))
+score_surf = test_font.render('My Game' , False , 'Black').convert()
+score_rect = score_surf.get_rect(center = (350, 50))
+
+
+snail_surf = pygame.image.load('snail(1).png').convert_alpha() # alpha removes unnecessary background
+snail_rect = snail_surf.get_rect(topright = (700,280))
+
+
+player_surf = pygame.image.load('gameCharacter.png').convert_alpha()
+player_rect = player_surf.get_rect(topleft = (80, 180))
 
 # CHECKPOINT 3 --> Basic animation
 
@@ -32,22 +37,54 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(ground_surface, (0, 0))
-    screen.blit(text_surface, (240 , 50))
+        # if event.type == pygame.MOUSEMOTION:
+        #     print(event.pos)        # prints mouse pos (x,y)
+        # if event.type == pygame.MOUSEBUTTONDOWN :
+        #     print("Button Down")      # prints when button pressed
+
+        # if event.type == pygame.MOUSEBUTTONUP:
+        #     print("Button up")        # prints when button release
+
+        # if event.type == pygame.MOUSEMOTION :
+        #     if player_rect.collidepoint(event.pos) :
+        #         print("Collision")          # prints when above the player character
+
+
+    screen.blit(ground_surf, (0, 0))
+    #CHECKPOINT 5 --> Drawing with Rectangles
+
+    #draw module
+    pygame.draw.rect(screen , 'Pink' , score_rect)
+    pygame.draw.rect(screen , 'Pink' , score_rect , 15)
+    pygame.draw.ellipse(screen , 'Brown' , pygame.Rect( ))
+
+
+
+    screen.blit(score_surf, score_rect)
 
     # snail animation condition 
 
-    #CHECKPOINT 4 --> Rectangles
-    snail_rectangle.x -= 2 
-    if snail_rectangle.right  <= 0 :
-        snail_rectangle.left = 800
+    #CHECKPOINT 4 --> Rectangles       ::  for positioning and collisions
 
-    screen.blit(snail_surface, snail_rectangle)
-    screen.blit(player_surface, player_rectangle)
+    snail_rect.x -= 2 
+    if snail_rect.right  <= 0 :
+        snail_rect.left = 800
+
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
+
+    #CHECKPOINT 5 --> Collision with Rectangle 
+    # if player_rect.colliderect(snail_rect) :
+    #     print("collision")
+
+    #CHECKPOINT 6 --> Collide Points
+    mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos) :
+    #     print(pygame.mouse.get_pressed())
 
 
-    # print(player_rectangle.left)    # rectangle position 80 from left 
-    snail_rectangle.left -= 4 
+    # print(player_rect.left)    # rectangle position 80 from left 
+    snail_rect.left -= 4 
     
 
     pygame.display.update()
